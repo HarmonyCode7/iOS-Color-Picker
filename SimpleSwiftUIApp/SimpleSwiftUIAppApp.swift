b497b78d303b5a5c68bf7fb4af3c7e795e1e6a21
Harmony Moyo <harmonymoyo17@gmail.com>
2021-09-18 04:39:51 +0100

    Initial Commit

---
+++ SimpleSwiftUIApp/SimpleSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleSwiftUIAppApp.swift
+//  SimpleSwiftUIApp
+//
+//  Created by Harmony on 18/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct SimpleSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
